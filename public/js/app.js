9b57a0eb8ed2af598ed57aa98ea4cede26c9b608
Chan Yik Hong <chanyikhong@Chans-MacBook-Pro.local>
2016-03-11 23:13:19 +0800

    Add front-end to submit message

--- public/js/app.js
+++ public/js/app.js
@@ -5,6 +5,21 @@ socket.on('connect', function() {
 });
 
 socket.on('message', function(message) {
-	console.log('New message:');
+	console.log('New message: ');
 	console.log(message.text);
+});
+
+// Handles submitting of new message
+var $form = jQuery('#message-form');
+
+$form.on('submit', function (event) {
+	event.preventDefault();
+
+	var $message = $form.find('input[name=message]');
+	socket.emit('message', {
+		text: $message.val()
+	});
+
+	$message.val('');
+	
 });
