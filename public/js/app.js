38a9d8ff7e5c97bb0b15806efdf431b45046c1c4
Chan Yik Hong <chanyikhong@Chans-MacBook-Pro.local>
2016-04-09 16:25:32 +0800

    Add styles for chat app

--- public/js/app.js
+++ public/js/app.js
@@ -15,12 +15,16 @@ jQuery('.room-title').text('Room Name: ' + room);
 
 socket.on('message', function(message) {
 	var momentTimestamp = moment.utc(message.timestamp);
-	var $message = jQuery('.messages');
+	var $messages = jQuery('.messages');
+	var $message = jQuery('<li class="list-group-item"></li>');
+
+
 	console.log('New message: ');
 	console.log(message.text);
 
 	$message.append('<p><b>' + message.name + ' ' + momentTimestamp.local().format('h:mm a') + '</b></p>');
 	$message.append('<p>' + message.text + '</p>');
+	$messages.append($message);
 });
 
 // Handles submitting of new message
